abc658bc1e0ad52d98104b0918082d92ee29934f
Stephen Sorensen <shuoink@gmail.com>
2020-01-04 22:59:23 -0500

    fix: add/configure missing semantic-release plugins

--- src/index.ts
+++ src/index.ts
@@ -289,11 +289,15 @@ const getPackageConfig = (options: Options) => {
     release: options.semanticRelease
       ? {
           plugins: [
+            '@semantic-release/commit-analyzer',
+            '@semantic-release/release-notes-generator',
             '@semantic-release/changelog',
+            '@semantic-release/npm',
+            '@semantic-release/github',
             [
               '@semantic-release/git',
               {
-                assets: ['package.json'],
+                assets: ['package.json', 'CHANGELOG.md'],
                 message:
                   // eslint-disable-next-line no-template-curly-in-string
                   'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
